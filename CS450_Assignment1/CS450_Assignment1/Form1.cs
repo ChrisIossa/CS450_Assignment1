4b2f8080b924204e4df72589fef2b3672bc975c2
ChrisIossa <chris.iossa@gmail.com>
2017-11-08 18:46:45 -0500

    Made a bunch of changes. About to remove if statement that prints first process execution time before adding other processes. Maintaing that just became a massive headache

--- CS450_Assignment1/CS450_Assignment1/Form1.cs
+++ CS450_Assignment1/CS450_Assignment1/Form1.cs
@@ -13,11 +13,11 @@ namespace CS450_Assignment1
     public partial class Form1 : Form
     {
         //private member variables
-        private int maxProc = 0;
-        private int waitProc = 0;
-        private int currTime = 0;
+        private int maxProc = 10; //number of processes specefied by the user
+        private int totalSeconds = 0;
+        //private int currTime = 0;
         LinkedList<Process> processesLL;
-        
+        Random randGen = new Random(); //RNG needed to randomly generate values
 
         public Form1()
         {
@@ -26,20 +26,42 @@ namespace CS450_Assignment1
 
         private void btnRun_Click(object sender, EventArgs e)
         {
-            
+            if(rbtnFCFS.Checked)
+            {
+                fcfsAlg();
+            }
         }
         
         private void fcfsAlg()
         {
-            Random randGen = new Random();
-            Queue<Process> processQueue;
+            
+            Queue<Process> processQueue = new Queue<Process>(); //queue to store processes waiting for CPU
+            DateTime internalClock;
             for (int i = 0; i < maxProc; i++)
             {
-                processQueue.Enqueue(new Process(String.Concat("Process ", (i + 1).ToString()), randGen.Next(1, 100), i));
-                System.Console.WriteLine("Added Process " + i+1 + ".");
-                System.Console.WriteLine("Priority: " + i + ".");
-                System.Console.WriteLine("Burst Time:" )
+                if (i == 0)
+                {
+                    processQueue.Enqueue(new Process(String.Concat("Process ", (i + 1).ToString()), randGen.Next(1, 100), i));
+                }
+                else
+                {
+                    processQueue.Enqueue(new Process(String.Concat("Process ", (i + 1).ToString()), randGen.Next(1, 100), i, processQueue.ElementAt(i-1).getTurnaroundTime()));
+                }
+                
+                txtConsole.AppendText("["+processQueue.ElementAt(i).getArrvTime().ToString("h:mm:ss.fff tt") + "] Added " + processQueue.ElementAt(i).getProcName() + ".\n");
+                txtConsole.AppendText("\t            Priority: " + processQueue.ElementAt(i).getPriority() + ".\n");
+                txtConsole.AppendText("\t            Burst Time: " + processQueue.ElementAt(i).getBurstTime() +"\n");
+                
+                txtConsole.AppendText("\n");
+                if (i == 0)
+                {
+                    internalClock = processQueue.Peek().getArrvTime();
+                    txtConsole.AppendText("[" + processQueue.Peek().getArrvTime().ToString("h:mm:ss.fff tt") + "] " + processQueue.Peek().getProcName() + " now executing.\n");
+                    txtConsole.AppendText("[" + processQueue.Peek().getArrvTime().ToString("h:mm:ss.fff tt") + "] " + processQueue.Peek().getProcName() + " now finished.\n"")
+
+                }
             }
+           
         }
     }
 }
--- CS450_Assignment1/CS450_Assignment1/Process.cs
+++ CS450_Assignment1/CS450_Assignment1/Process.cs
@@ -9,18 +9,28 @@ namespace CS450_Assignment1
     class Process
     {
         //empty constructor, never used by me
-        Process()
+        public Process()
         {
 
         }
 
         //constructor with varaibles passed as parameters
-        Process(String nameIn, int burstIn, int priorIn)
+        public Process(String nameIn, int burstIn, int priorIn)
         {
             procName = nameIn;
             burstTime = burstIn;
             priority = priorIn;
             arrvTime = DateTime.Now;
+            waitTime = arrvTime;
+        }
+
+        public Process(String nameIn, int burstIn, int priorIn, DateTime waitIn)
+        {
+            procName = nameIn;
+            burstTime = burstIn;
+            priority = priorIn;
+            arrvTime = DateTime.Now;
+            waitTime = waitIn;
         }
 
 
@@ -46,6 +56,11 @@ namespace CS450_Assignment1
             arrvTime = arrvIn;
         }
 
+        public void setWaitTime(DateTime waitIn)
+        {
+            waitTime = waitIn;
+        }
+
         // getters for member variables
 
         public String getProcName()
@@ -68,12 +83,33 @@ namespace CS450_Assignment1
             return arrvTime;
         }
 
+        public DateTime getWaitTime()
+        {
+            return waitTime;
+        }
 
+        public int getWaitTimeSec()
+        {
+            return (waitTime.Subtract(arrvTime).Seconds);
+        }
+
+        public DateTime getTurnaroundTime()
+        {
+            if (waitTime != arrvTime)
+            {
+                return waitTime.AddSeconds(burstTime);
+            }
+            else
+            {
+                return arrvTime.AddSeconds(burstTime);
+            }
+        }
 
         //member variables
         private String procName = "";
         private int burstTime = 0;
         private int priority = 0;
         private DateTime arrvTime;
+        private DateTime waitTime;
     }
 }
